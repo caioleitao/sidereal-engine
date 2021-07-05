1674d6e0a4c2653c2af8ecf79945263716e73f8b
Caio Cesar <kylespace@icloud.com>
2021-07-05 18:10:48 -0300

    add frond end

--- librarytest.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Kyle on 7/4/21.
-//
-
-#include "libs/librarytest.h"
-
-int Examplesum::sum(int a, int b){
-    return a + b;
-}
--- libs/librarytest.h
+++
@@ -1,7 +0,0 @@
-//
-// Created by Kyle on 7/4/21.
-//
-
-class Examplesum{
-public: int sum(int a, int b);
-};
