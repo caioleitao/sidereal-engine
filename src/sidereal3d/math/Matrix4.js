0b10e981b6b6ff3bd6202a8d72c9f54c33f6732a
Caio Cesar <kylespace@icloud.com>
2021-07-07 02:54:49 -0300

    matrix4

--- src/sidereal3d/math/Matrix4.js
+++ src/sidereal3d/math/Matrix4.js
@@ -138,8 +138,168 @@ class Matrix4 {
         const e = Math.cos(z), f = Math.sin(z);
 
         if (euler.order === 'XYZ') {
+            const ae = a * e, af = a * f, be = b * e, bf = b * f;
 
+            te[0] = c * e;
+            te[4] = - c * f;
+            te[8] = d;
+
+            te[1] = af + be * d;
+            te[5] = ae - bf * d;
+            te[9] = - b * c;
+
+            te[2] = bf - ae * d;
+            te[6] = be + af * d;
+            te[10] = a * c;
+        } else if (euler.order === 'YXZ') {
+            const ce = c * e, cf = c * f, de = d * e, df = d * f;
+
+            te[0] = ce + df * b;
+            te[4] = de * b - cf;
+            te[8] = a * d;
+
+            te[1] = a * f;
+            te[5] = a * e;
+            te[9] = - b;
+
+            te[2] = cf * b - de;
+            te[6] = df + ce * b;
+            te[10] = a * c;
+        } else if (euler.order === 'ZXY') {
+            const ce = c * e, cf = c * f, de = d * e, df = d * f;
+
+            te[0] = ce - df * b;
+            te[4] = - a * f;
+            te[8] = de + cf * b;
+
+            te[1] = cf + de * b;
+            te[5] = a * e;
+            te[9] = df - ce * b;
+
+            te[2] = - a * d;
+            te[6] = b;
+            te[10] = a * b;
+        } else if (euler.order === 'ZYX') {
+            const ae = a * e, af = a * f, be = b * e, bf = b * f;
+
+            te[0] = c * e;
+            te[4] = be * d - af;
+            te[8] = ae * d + bf;
+
+            te[1] = c * f;
+            te[5] = bf * d + ae;
+            te[9] = af * d - be;
+
+            te[2] = - d;
+            te[6] = b * c;
+            te[10] = a * c;
+        } else if (euler.order === 'YZX') {
+            const ac = a * c, ad = a * d, bc = b * c, bd = b * d;
+
+            te[0] = c * e;
+            te[4] = bd - ac * f;
+            te[8] = bc * f + ad;
+
+            te[1] = f;
+            te[5] = a * e;
+            te[9] = - b * e;
+
+            te[2] = - d * e;
+            te[6] = ad * f + bc;
+            te[10] = ac - bd * f;
+        } else if (euler.order === 'XZY') {
+            const ac = a * c, ad = a * d, bc = b * c, bd = b * d;
+
+            te[0] = c * e;
+            te[4] = - f;
+            te[8] = d * e;
+
+            te[1] = ac * f + bd;
+            te[5] = a * e;
+            te[9] = ad * f - bc;
+
+            te[2] = bc * f - ad;
+            te[6] = b * e;
+            te[10] = bd * f + ac;
         }
+
+        te[3] = 0;
+        te[7] = 0;
+        te[11] = 0;
+
+        te[12] = 0;
+        te[13] = 0;
+        te[14] = 0;
+        te[15] = 0;
+
+        return this;
+    }
+
+    makeRotationFromQuaternion(q) {
+        return this.compose(_zero, q, _one);
+    }
+
+    lookAt(eye,target,up) {
+        const te = this.elements;
+
+        _z.subVectors(eye, target);
+
+        if (_z.lengthSq() === 0) {
+            _z.z = 1;
+        }
+
+        _z.normalize();
+        _x.crossVectors(up, _z);
+
+        if (_x.length() === 0) {
+            if (Math.abs(up.z) === 1) {
+                _z.x += 0.0001;
+            } else {
+                _z.z += 0.0001;
+            }
+
+            _z.normalize();
+            _x.crossVectors(up,_z);
+        }
+
+        _x.normalize();
+        -y.crossVectors(_z, _x);
+
+        te[0] = _x.x; te[4] = _y.x; te[8] = _z.x;
+        te[1] = _x.y; te[5] = _y.y; te[9] = _z.y;
+        te[2] = _x.z; te[6] = _y.z; te[10] = _z.z;
+
+        return this;
+    }
+
+    multiply(m,n) {
+        if (n !== undefined) {
+            console.warn('msg');
+            return this.multiplyMatrices(m,n);
+        }
+        return this.multiplyMatrices(this, m);
+    }
+
+    premultiply(m) {
+        return this.multiplyMatrices(m, this);
+    }
+
+    multiplyMatrices(a,b) {
+        const ae = a.elements;
+        const be = b.elements;
+        const te = this.elements;
+
+        const a11 = ae[0], a12 = ae[4], a13 = ae[8], a14 = ae[12];
+        const a21 = ae[1], a22 = ae[5], a23 = ae[9], a24 = ae[13];
+        const a31 = ae[2], a32 = ae[6], a33 = ae[10], a34 = ae[14];
+        const a41 = ae[3], a42 = ae[7], a43 = ae[11], a44 = ae[15];
+
+        const b11 = be[0], b12 = be[4], b13 = be[8], b14 = be[12];
+        const b21 = be[1], b22 = be[5], b23 = be[9], b24 = be[13];
+        const b31 = be[2], b32 = be[6], b33 = be[10], b34 = be[14];
+        const b41 = be[3], b42 = be[7], b43 = be[11]. b44 = be[15];
+
+        // imcomplete! finish today!!!
     }
 
 }
